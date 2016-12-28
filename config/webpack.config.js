dd286e547a24718f620bcf564fa83619e14ce5ed
Romain FENNINGER <zipou@Mac-Zi.local>
2016-12-28 10:22:40 +0100

    Updates

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     './src/index.jsx' // Your appʼs entry point
   ],
   output: {
-    path: path.join(__dirname, '../../dist/'),
+    path: path.join(__dirname, '../'),
     filename: 'index.js',
   },
   resolve: {
